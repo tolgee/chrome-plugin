7920aff9f8ab94e7b5c69e0dd94d495ed5234e71
Štěpán Granát <granat.stepan@gmail.com>
2024-10-18 13:50:33 +0200

    fix: hotfix manifest.json to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,47 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import zipPack from 'vite-plugin-zip-pack';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import fs from 'node:fs';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import path from 'node:path';
 
 import manifest from './manifest.json';
 
+function myPlugin(): Plugin {
+  function updateManifest() {
+    const absolutePath = path.resolve('./dist/manifest.json');
+
+    const content = fs.readFileSync(absolutePath);
+    const test = JSON.parse(content);
+    test.web_accessible_resources = test.web_accessible_resources.map((i) => ({
+      ...i,
+      use_dynamic_url: false,
+    }));
+    fs.writeFile(absolutePath, JSON.stringify(test, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('manifest.json updated');
+      }
+    });
+  }
+
+  return {
+    name: 'my-plugin',
+    generateBundle(options, bundle) {
+      console.log(bundle);
+    },
+    writeBundle() {
+      updateManifest();
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest: manifest as any }), zipPack()],
+  plugins: [react(), crx({ manifest: manifest as any }), myPlugin(), zipPack()],
 });
