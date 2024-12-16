fff26d98c78248aab061964c8374c48b65fad79e
Lahgolz <lahgolz@proton.me>
2024-12-16 11:39:04 +0100

    fix: remove myPlugin from firefox vite config

--- vite.config.firefox.ts
+++ vite.config.firefox.ts
@@ -1,45 +1,9 @@
-import { defineConfig, Plugin } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
 import zipPack from 'vite-plugin-zip-pack';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import fs from 'node:fs';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import path from 'node:path';
 
 import manifest from './manifest.json';
-
-function myPlugin(): Plugin {
-  function updateManifest() {
-    const absolutePath = path.resolve('./dist-firefox/manifest.json');
-
-    const content = fs.readFileSync(absolutePath);
-    const test = JSON.parse(content);
-    test.web_accessible_resources = test.web_accessible_resources.map((i) => ({
-      ...i,
-      use_dynamic_url: false,
-    }));
-    fs.writeFile(absolutePath, JSON.stringify(test, null, 2), (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('manifest.json updated');
-      }
-    });
-  }
-
-  return {
-    name: 'my-plugin',
-    generateBundle(options, bundle) {
-      console.log(bundle);
-    },
-    writeBundle() {
-      updateManifest();
-    },
-  };
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -50,7 +14,6 @@ export default defineConfig({
   plugins: [
     react(),
     crx({ manifest: manifest as any, browser: 'firefox' }),
-    myPlugin(),
     zipPack({ inDir: 'dist-firefox', outFileName: 'firefox.zip' }),
   ],
 });
